6c49e463bf9b5570bc1be6e7057f6255912da643
rachennamwong <refresh594@gmail.com>
2020-03-11 19:22:01 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'fDeIoqvoJ6K6Y4XTGHBhKq+yee31qJRwn2fe7Q62OEIF13z7gYnMaNtkxK5bmKrRB+LX7FJZFa0vRiv34QB3IXUgw1een8xyad3HzWMM9+S983CtS1DUWfGVFngcR+KqsueGdlnXsfiqI03yA4lJZgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
